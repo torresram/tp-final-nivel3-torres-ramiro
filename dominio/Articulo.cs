4f724cc73efa644c8215019887bf2bc2b6559342
torresram <ramirotorres90@yahoo.com.ar>
2023-12-28 03:30:35 -0300

    Clases

--- dominio/Articulo.cs
+++ dominio/Articulo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -12,15 +13,14 @@ namespace dominio
     {
         public int Id { get; set; }
         [DisplayName("Código")]
-        public string Codigo { get; set; }        
+        public string Codigo { get; set; }
         public string Nombre { get; set; }
         [DisplayName("Descripción")]
-        public string Descripcion { get; set; }        
+        public string Descripcion { get; set; }
         public Marca Marca { get; set; }
         [DisplayName("Categoría")]
         public Categoria Categoria { get; set; }
         public string ImagenURL { get; set; }
         public decimal Precio { get; set; }
-
     }
 }
---
+++ dominio/Favoritos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Favoritos
+    {
+        public int id { get; set; }
+        public Usuario Usuario { get; set; }
+        public Articulo Articulo { get; set; }
+    }
+}
