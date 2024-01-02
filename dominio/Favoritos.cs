91b5ee6dd907c6b75e3a0aa80eadf8adf731718a
torresram <ramirotorres90@yahoo.com.ar>
2024-01-01 22:51:06 -0300

    Clases

--- dominio/Favoritos.cs
+++ dominio/Favoritos.cs
@@ -8,7 +8,7 @@ namespace dominio
 {
     public class Favoritos
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public Usuario Usuario { get; set; }
         public Articulo Articulo { get; set; }
     }
