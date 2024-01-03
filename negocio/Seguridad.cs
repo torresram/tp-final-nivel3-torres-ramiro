e10421e22ef2d95882a33d471c5d8a370e6db5dd
torresram <ramirotorres90@yahoo.com.ar>
2024-01-03 01:55:30 -0300

    Métodos

---
+++ negocio/Seguridad.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public static class Seguridad
+    {
+        public static bool sesionActiva(object user)
+        {
+            Usuario usuario = user != null ? (Usuario)user : null;
+            if(usuario != null && usuario.Id != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool esAdmin(object user)
+        {
+            Usuario usuario = user != null ? (Usuario)user : null;
+            return usuario != null ? usuario.EsAdmin : false;
+        }
+    }
+}
