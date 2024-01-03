6d95c37a8d6376548df4a3c46a6854d656bd3f9a
torresram <ramirotorres90@yahoo.com.ar>
2024-01-03 01:53:09 -0300

    Consultas SQL

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
---select * from ARTICULOS
+ --select * from ARTICULOSj
 --select * from CATEGORIAS
 --select * from FAVORITOS
 --select * from MARCAS
@@ -22,29 +21,7 @@
 
 --INSERT INTO FAVORITOS (IdUser, IdArticulo) VALUES (1,2)
 
-create procedure storedFavoritos @id int as
-SELECT F.Id, U.email Usuario,A.Nombre Articulo 
-FROM FAVORITOS F, USERS U, ARTICULOS A 
-WHERE U.Id = F.IdUser and A.Id = F.IdArticulo and U.Id = @id
-=======
---select * from ARTICULOS
---select * from CATEGORIAS
---select * from FAVORITOS
---select * from MARCAS
---select * from USERS
-
---CREATE PROCEDURE storedListar as
---SELECT A.Id, A.Codigo, A.Nombre, A.Descripcion, M.Descripcion Marca, C.Descripcion Categoria, A.ImagenUrl, A.IdMarca, A.IdCategoria, A.Precio 
---FROM ARTICULOS A, MARCAS M, CATEGORIAS C 
---WHERE A.IdMarca = M.Id and A.IdCategoria = C.Id
-
---insert into ARTICULOS (Codigo, Nombre,Descripcion,IdMarca, IdCategoria, ImagenUrl,Precio)
---VALUES ('ABC', 'River', 'Plate', 3,1,'no hay', 3452.6457)
-
---delete from ARTICULOS where id = 6
-
---create procedure storedArticulo @id int as
---SELECT A.Id, A.Codigo, A.Nombre, A.Descripcion, M.Descripcion Marca, C.Descripcion Categoria, A.ImagenUrl, A.IdMarca, A.IdCategoria, A.Precio 
---FROM ARTICULOS A, MARCAS M, CATEGORIAS C 
---WHERE A.IdMarca = M.Id and A.IdCategoria = C.Id and A.Id = @id
->>>>>>> 5b54c0b8513a1c75bbf2d73ab4c73f8ab76a7eb4
+--create procedure storedFavoritos @id int as
+--SELECT F.Id, U.email Usuario,A.Nombre Articulo 
+--FROM FAVORITOS F, USERS U, ARTICULOS A 
+--WHERE U.Id = F.IdUser and A.Id = F.IdArticulo and U.Id = @id
