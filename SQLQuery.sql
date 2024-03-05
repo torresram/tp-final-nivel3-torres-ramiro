4bcd720084b2b6d1881be0ef2366c70b5d02607c
torresram <ramirotorres90@yahoo.com.ar>
2024-03-05 20:02:46 -0300

    Consultas SQL

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -6,6 +6,7 @@
 
 --UPDATE USERS SET urlImagenPerfil = '' WHERE Id = 1
 --UPDATE USERS SET pass = '' WHERE Id = 1
+--UPDATE USERS SET nombre = '', apellido = '' WHERE id = 1
 
 --CREATE PROCEDURE storedListar as
 --SELECT A.Id, A.Codigo, A.Nombre, A.Descripcion, M.Descripcion Marca, C.Descripcion Categoria, A.ImagenUrl, A.IdMarca, A.IdCategoria, A.Precio 
@@ -24,11 +25,6 @@
 
 --INSERT INTO FAVORITOS (IdUser, IdArticulo) VALUES (1,2)
 
---create procedure storedFavoritos @id int as
---SELECT F.Id, U.email Usuario,A.Nombre Articulo 
---FROM FAVORITOS F, USERS U, ARTICULOS A 
---WHERE U.Id = F.IdUser and A.Id = F.IdArticulo and U.Id = @id
-
 --CREATE PROCEDURE storedAgregar 
 --@codigo varchar(50), @nombre varchar (50), @descripcion varchar (150), @idMarca int, @idCategoria int,
 --@imagenUrl varchar(1000), @precio money as
@@ -44,4 +40,12 @@
 --alter procedure nuevoUsuario @email varchar(100), @pass varchar(20) as
 --INSERT INTO USERS (email, pass, admin) output inserted.id VALUES (@email, @pass, 0)
 
---SELECT Id, email, pass,nombre,apellido, urlImagenPerfil, admin FROM USERS WHERE email='' and pass = ''
+--SELECT Id, email, pass,nombre,apellido, urlImagenPerfil, admin FROM USERS WHERE email= '' and pass = ''
+
+--CREATE PROCEDURE storedFavs @id int as
+--SELECT F.IdUser, F.IdArticulo,A.Codigo, A.Nombre, A.Descripcion, A.ImagenUrl,A.Precio,C.Descripcion Categoria, M.Descripcion Marca
+--from FAVORITOS F, ARTICULOS A, MARCAS M, CATEGORIAS C
+--WHERE F.IdArticulo = A.Id and M.Id = A.IdMarca and A.IdCategoria = C.Id and F.IdUser = @id
+
+--INSERT INTO FAVORITOS (IdUser, IdArticulo) VALUES (1,3)
+--DELETE FROM FAVORITOS WHERE IdUser = 1 and IdArticulo = 3
